fabe143617f3a7b7bbf6d3dba3f43973597f2cc0
paypulse <sourcedev5@gmail.com>
2017-11-03 18:47:08 +0900

    Before Testing

--- SerialPortEx/Form1.Designer.cs
+++ SerialPortEx/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SerialPortEx
 {
-    partial class Form1
+    partial class SerialComm_Load
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,12 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbSettings = new System.Windows.Forms.GroupBox();
+            this.cbHandShake = new System.Windows.Forms.ComboBox();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.cbDataSize = new System.Windows.Forms.ComboBox();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
+            this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbRecvMessage = new System.Windows.Forms.RichTextBox();
+            this.tbSendMessage = new System.Windows.Forms.TextBox();
+            this.btSendMessage = new System.Windows.Forms.Button();
+            this.btConnectControl = new System.Windows.Forms.Button();
+            this.gbSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSettings
+            // 
+            this.gbSettings.Controls.Add(this.cbHandShake);
+            this.gbSettings.Controls.Add(this.cbParity);
+            this.gbSettings.Controls.Add(this.cbDataSize);
+            this.gbSettings.Controls.Add(this.cbBaudRate);
+            this.gbSettings.Controls.Add(this.cbComPort);
+            this.gbSettings.Controls.Add(this.label5);
+            this.gbSettings.Controls.Add(this.label4);
+            this.gbSettings.Controls.Add(this.label3);
+            this.gbSettings.Controls.Add(this.label2);
+            this.gbSettings.Controls.Add(this.label1);
+            this.gbSettings.Location = new System.Drawing.Point(13, 13);
+            this.gbSettings.Name = "gbSettings";
+            this.gbSettings.Size = new System.Drawing.Size(320, 202);
+            this.gbSettings.TabIndex = 0;
+            this.gbSettings.TabStop = false;
+            this.gbSettings.Text = "Settings";
+            // 
+            // cbHandShake
+            // 
+            this.cbHandShake.FormattingEnabled = true;
+            this.cbHandShake.Items.AddRange(new object[] {
+            "none",
+            "Xon/Xoff",
+            "request to send",
+            "request to send Xon/Xoff"});
+            this.cbHandShake.Location = new System.Drawing.Point(129, 170);
+            this.cbHandShake.Name = "cbHandShake";
+            this.cbHandShake.Size = new System.Drawing.Size(177, 20);
+            this.cbHandShake.TabIndex = 9;
+            // 
+            // cbParity
+            // 
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Items.AddRange(new object[] {
+            "none",
+            "even",
+            "mark",
+            "odd",
+            "space"});
+            this.cbParity.Location = new System.Drawing.Point(129, 135);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(177, 20);
+            this.cbParity.TabIndex = 8;
+            // 
+            // cbDataSize
+            // 
+            this.cbDataSize.FormattingEnabled = true;
+            this.cbDataSize.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6"});
+            this.cbDataSize.Location = new System.Drawing.Point(129, 100);
+            this.cbDataSize.Name = "cbDataSize";
+            this.cbDataSize.Size = new System.Drawing.Size(177, 20);
+            this.cbDataSize.TabIndex = 7;
+            // 
+            // cbBaudRate
+            // 
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Items.AddRange(new object[] {
+            "115200",
+            "19200",
+            "38400",
+            "57600",
+            "9600"});
+            this.cbBaudRate.Location = new System.Drawing.Point(129, 68);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(177, 20);
+            this.cbBaudRate.TabIndex = 6;
+            // 
+            // cbComPort
+            // 
+            this.cbComPort.FormattingEnabled = true;
+            this.cbComPort.Location = new System.Drawing.Point(129, 34);
+            this.cbComPort.Name = "cbComPort";
+            this.cbComPort.Size = new System.Drawing.Size(177, 20);
+            this.cbComPort.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "handshake";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "parity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "baudrate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "port";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(362, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Messages";
+            // 
+            // tbRecvMessage
+            // 
+            this.tbRecvMessage.Location = new System.Drawing.Point(365, 50);
+            this.tbRecvMessage.Name = "tbRecvMessage";
+            this.tbRecvMessage.Size = new System.Drawing.Size(291, 199);
+            this.tbRecvMessage.TabIndex = 2;
+            this.tbRecvMessage.Text = "";
+            // 
+            // tbSendMessage
+            // 
+            this.tbSendMessage.Location = new System.Drawing.Point(365, 266);
+            this.tbSendMessage.Name = "tbSendMessage";
+            this.tbSendMessage.Size = new System.Drawing.Size(208, 21);
+            this.tbSendMessage.TabIndex = 3;
+            // 
+            // btSendMessage
+            // 
+            this.btSendMessage.Location = new System.Drawing.Point(581, 264);
+            this.btSendMessage.Name = "btSendMessage";
+            this.btSendMessage.Size = new System.Drawing.Size(75, 23);
+            this.btSendMessage.TabIndex = 4;
+            this.btSendMessage.Text = "Send";
+            this.btSendMessage.UseVisualStyleBackColor = true;
+            this.btSendMessage.Click += new System.EventHandler(this.btSendMessage_Click);
+            // 
+            // btConnectControl
+            // 
+            this.btConnectControl.Location = new System.Drawing.Point(13, 225);
+            this.btConnectControl.Name = "btConnectControl";
+            this.btConnectControl.Size = new System.Drawing.Size(320, 62);
+            this.btConnectControl.TabIndex = 5;
+            this.btConnectControl.Text = "연결";
+            this.btConnectControl.UseVisualStyleBackColor = true;
+            this.btConnectControl.Click += new System.EventHandler(this.btConnectControl_Click);
+            // 
+            // SerialComm_Load
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 311);
+            this.Controls.Add(this.btConnectControl);
+            this.Controls.Add(this.btSendMessage);
+            this.Controls.Add(this.tbSendMessage);
+            this.Controls.Add(this.tbRecvMessage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.gbSettings);
+            this.Name = "SerialComm_Load";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SerialComm_Load_Load);
+            this.gbSettings.ResumeLayout(false);
+            this.gbSettings.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbSettings;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbHandShake;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.ComboBox cbDataSize;
+        private System.Windows.Forms.ComboBox cbBaudRate;
+        private System.Windows.Forms.ComboBox cbComPort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox tbRecvMessage;
+        private System.Windows.Forms.TextBox tbSendMessage;
+        private System.Windows.Forms.Button btSendMessage;
+        private System.Windows.Forms.Button btConnectControl;
     }
 }
 
--- SerialPortEx/Form1.cs
+++ SerialPortEx/Form1.cs
@@ -7,14 +7,167 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+
 
 namespace SerialPortEx
 {
-    public partial class Form1 : Form
+    public partial class SerialComm_Load : Form
     {
-        public Form1()
+        public SerialComm_Load()
         {
             InitializeComponent();
+        }
+
+        //serial port를 관리할 객체를 생성 property
+        private SerialPort _Port;
+        private SerialPort Port
+        {
+            get
+            {
+                if(_Port == null)
+                {
+                    _Port = new SerialPort();
+                    _Port.PortName = "COM1";
+                    _Port.BaudRate = 9600;
+                    _Port.DataBits = 8;
+                    _Port.Parity = Parity.None;
+                    _Port.Handshake = Handshake.None;
+                    _Port.StopBits = StopBits.One;
+                    _Port.Encoding = Encoding.UTF8;
+                    _Port.DataReceived += Port_DataReceived;
+
+                }
+                return _Port;
+            }
+        }
+
+        //시리얼 포트를 통해 데이터가 수신되면 발생하는 이벤트 
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //현재 수신된 데이터를 버퍼에서 몽땅 가져 온다
+            String msg = Port.ReadExisting();
+
+            //대리자를 통해 현재 폼의 상태를 변경 시킬때 주로 쓴다
+            // 왜 바로 컨트롤의 상태를 바꿔주면 안되냐 하면 다른 
+            //쓰레드 끼리는 서로의 데이터 안전성을 위해 바로 접근 할 수 없다.
+            //즉, 수신 쓰레드에서 폼의 상태를 변경하라고 대리자를 통해 시켜두면, 메인 쓰레드 차례가
+            //돌아 왔을때 이일을 처리하게 되는 것이다. 
+            this.Invoke(new EventHandler(delegate
+            {
+                Strings = String.Format("[RECV] {0}", msg);
+            }));
+
+        }
+
+
+        //상태 표시를 위한 속성 Property : 시리얼 포트 상태및 컨트롤 제어
+        private Boolean IsOpen
+        {
+            get { return Port.IsOpen; }
+            set
+            {
+                if(value)
+                {
+                    Strings = "연결 됨";
+                    btConnectControl.Text = "연결 끊기";
+                    tbRecvMessage.Enabled = true;
+                    tbSendMessage.Enabled = true;
+                    btSendMessage.Enabled = true;
+                    gbSettings.Enabled = false;
+                }
+                else
+                {
+                    Strings = "연결 해제 됨";
+                    btConnectControl.Text = "연결";
+                    tbRecvMessage.Enabled = false;
+                    tbSendMessage.Enabled = false;
+                    btSendMessage.Enabled = false;
+                    gbSettings.Enabled = true;
+                }
+            }
+        }
+
+        /*로그 제어*/
+        private StringBuilder _Strings;
+        private String Strings
+        {
+            set
+            {
+                if(_Strings == null)
+                {
+                    _Strings = new StringBuilder(1024);
+                }
+
+                //로그 길이가 1024자가 되면, 이전 로그 삭제
+                if (_Strings.Length >= (1024 - value.Length))
+                {
+                    _Strings.Clear();
+                }
+
+                //로그 추가 및 화면 표시
+                _Strings.AppendLine(value);
+                tbRecvMessage.Text = _Strings.ToString();
+            }
+        }
+
+        private void SerialComm_Load_Load(object sender, EventArgs e)
+        {
+            //시리얼 포트 목록 갱신 --> 열려 있는 Comport를 가져 와라
+            cbComPort.DataSource = SerialPort.GetPortNames();
+
+            //기타 셋팅 목록 기본값 선택
+            cbBaudRate.SelectedItem = 0;
+            cbDataSize.SelectedItem = 0;
+            cbParity.SelectedItem = 0;
+            cbHandShake.SelectedItem = 0;
+
+
+        }
+
+        private void btConnectControl_Click(object sender, EventArgs e)
+        {
+            if(!Port.IsOpen)
+            {
+                // 현재 시리얼이 연결된 상태가 아니면 연결
+                Port.PortName = cbComPort.SelectedItem.ToString();
+                Port.BaudRate = Convert.ToInt32(cbBaudRate.SelectedItem);
+                Port.DataBits = Convert.ToInt32(cbDataSize.SelectedItem);
+                Port.Parity = (Parity)cbParity.SelectedIndex;
+                Port.Handshake = (Handshake)cbHandShake.SelectedItem;
+
+                try
+                {
+                    //연결
+                    Port.Open();
+                }catch (Exception ex) { Strings = String.Format("[ERR] {0}", ex.Message); }
+            }
+            else
+            {
+                // 현재 시리얼이 연결 상태이면, 연결 해제
+                Port.Close();
+            }
+
+            // 상태 변경
+            IsOpen = Port.IsOpen;
+        }
+
+        private void btSendMessage_Click(object sender, EventArgs e)
+        {
+            //보낼 메세지가 없으면 종료 
+            String text = tbSendMessage.Text.Trim();
+            if (String.IsNullOrEmpty(text)) return;
+
+            try
+            {
+                //메세지 전송
+                Port.WriteLine(text);
+                //표시
+                Strings = String.Format("[SEND] {0}", text);
+            }catch(Exception ex)
+            {
+                Strings = string.Format("[ERR] {0}", ex.Message);
+            }
         }
     }
 }
